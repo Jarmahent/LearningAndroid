80a01be12d2b53d8cc85e4d9a6c024b63b4be5fb
= <=>
2018-05-31 07:50:05 -0400

    Ui changes

--- app/src/main/java/io/nanoapp/rockzom/androidapp/landingarea.java
+++ app/src/main/java/io/nanoapp/rockzom/androidapp/landingarea.java
@@ -1,8 +1,10 @@
 package io.nanoapp.rockzom.androidapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class landingarea extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,7 +31,7 @@ public class landingarea extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Bitches will always be bitches", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -97,5 +101,13 @@ public class landingarea extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void alertSongPlay(View v){
+        TextView textview = (TextView) findViewById(R.id.songNameSearch);
+        String searchTerm = textview.getText().toString();
+        //System.out.println(searchTerm);
+        Snackbar.make(v, searchTerm, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
